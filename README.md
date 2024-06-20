cb5b315768d2371ff041bff8d7aab22a52b88f65
Khachik Bakunts <khachik_bakunts@epam.com>
2024-06-20 17:32:21 +0400

    add project

---
+++ README.md
@@ -0,0 +1,9 @@
+github automation
+=================
+
+C# and Java WebDriver test automation project for GitHub
+
+Java
+====
+
+Run from command line: mvn -Dbrowser=chrome -Dsurefire.suiteXmlFiles=src\test\resources\testng-smoke.xml -Denvironment=dev clean test
---
+++ src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -0,0 +1,32 @@
+package com.epam.ta.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverSingleton {
+
+    private static WebDriver driver;
+
+
+    private DriverSingleton(){}
+
+    public static WebDriver getDriver(){
+        if (null == driver){
+            if (System.getProperty("browser").equals("firefox")) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void closeDriver(){
+        driver.quit();
+        driver = null;
+    }
+}
